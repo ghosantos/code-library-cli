de822f770b499a9ad32aba6a01e8617b55813b31
ghosantos <gustavo.santos7611@gmail.com>
2026-03-05 11:57:32 -0300

    feat: configura infraestrutura de conexao com banco de dados

---
+++ src/db/DB.java
@@ -0,0 +1,32 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+
+    public static Connection getConnection(){
+        try {
+            Properties props = loadProperties();
+            String url = props.getProperty("url");
+            return DriverManager.getConnection(url, props);
+        }catch (SQLException e){
+            throw new DbException(e);
+        }
+    }
+
+    private static Properties loadProperties(){
+        try ( FileInputStream fs = new FileInputStream("src/resources/db.properties")){
+            Properties props = new Properties();
+            props.load(fs);
+            return props;
+        }catch (IOException e){
+            throw new DbException(e);
+        }
+    }
+
+}
